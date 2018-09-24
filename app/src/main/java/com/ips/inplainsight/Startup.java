fd0a79d2708dd8c84f5ddd807d9ac829d0f4f91a
Anthony Codella <codellaanthony@gmail.com>
2018-09-24 01:41:21 -0400

    game lobby files added

--- app/src/main/java/com/ips/inplainsight/Startup.java
+++ app/src/main/java/com/ips/inplainsight/Startup.java
@@ -1,2 +1,18 @@
-public class Startup {
+package com.ips.inplainsight;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.app.Activity;
+
+public class Startup extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+    }
 }
