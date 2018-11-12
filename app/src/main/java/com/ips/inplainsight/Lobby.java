9afae2bb613b294b041a62421e8035b70c3fd782
Anthony Codella <codellaanthony@gmail.com>
2018-11-11 19:42:55 -0500

    displays dummy data from firebase

--- app/src/main/java/com/ips/inplainsight/Lobby.java
+++ app/src/main/java/com/ips/inplainsight/Lobby.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.LinearLayoutManager;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
@@ -19,10 +20,9 @@ import java.util.ArrayList;
 
 public class Lobby extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
+    private RecyclerView mGameList;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-
     private DatabaseReference mDatabase;
 
     @Override
@@ -38,49 +38,65 @@ public class Lobby extends AppCompatActivity {
         //PlayerClass p = new PlayerClass("bob");
         //newGame.addPlayer(p);
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child("game1").setValue(game1);
-        mDatabase.child("game2").setValue(game2);
-        mDatabase.child("game3").setValue(game3);
+        mDatabase.child("games").push().setValue(game1);
+        mDatabase.child("games").push().setValue(game2);
+        mDatabase.push().child("games").setValue(game3);
         */
 
-
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
+        mGameList = (RecyclerView) findViewById(R.id.my_recycler_view);
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
+        mGameList.setHasFixedSize(true);
         // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mGameList.setLayoutManager(mLayoutManager);
         // specify an adapter (see also next example)
         //mAdapter = new RecyclerViewAdapter(myDataset);
-        mRecyclerView.setAdapter(mAdapter);
+        mGameList.setAdapter(mAdapter);
 
+        new GetDataFromFirebase().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
-        /*
-        private static final String TAG = "MainActivity";
+        // Read from the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference mRef = database.getReference("games");
 
-        //vars
-        private ArrayList<String> mNames = new ArrayList<>();
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_lobby);
-            Log.d(TAG, "onCreate: started.");
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                ArrayList<String> values = (ArrayList<String>) dataSnapshot.getValue();
+                mGameList.setAdapter(new RecyclerViewAdapter(values));
+            }
 
-        }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                System.out.println("Failed to read value." + error.toException());
+            }
+        });
 
-        private void initRecyclerView(){
-            Log.d(TAG, "initRecyclerView: init recyclerview.");
-            RecyclerView recyclerView = findViewById(R.id.recyclerv_view);
-            RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls);
-            recyclerView.setAdapter(adapter);
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        }
-        */
 
     }
+
+    private class GetDataFromFirebase extends AsyncTask<Void,Void,Boolean>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+        }
+    }
+
+
 }
